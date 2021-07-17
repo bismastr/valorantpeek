035a33006dd83098045bcabc78804bde676b04d5
atharianr <atharianrahmadani@gmail.com>
2021-07-17 21:11:33 +0700

    Splash Screen, UI Login and Register

---
+++ app/src/main/java/com/bismastr/myvalorant/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.bismastr.myvalorant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.transition.Fade;
+import android.transition.Transition;
+import android.transition.TransitionManager;
+import android.view.View;
+
+import com.bismastr.myvalorant.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private ActivitySplashBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Transition transition = new Fade();
+                transition.setDuration(600);
+                transition.addTarget(binding.llButton);
+                TransitionManager.beginDelayedTransition(binding.getRoot(), transition);
+
+            }
+        }, 3000);
+    }
+}
